a1bd3203070d4530dd3314e49b67225bc5e58bdd
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-05-29 16:58:34 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 Gotham Coder. Vigilantly built with React.</p>
+    </footer>
+  );
+};
+
+export default Footer;

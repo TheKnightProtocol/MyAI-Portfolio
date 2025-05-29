24a2b8c5f8fecbd4cdb00ed2b29749161e471bd4
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-05-29 20:55:48 +0530

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    // Uncomment below for advanced production optimizations
+    // 'postcss-nesting': {},  // Enables nesting like Sass
+    // 'cssnano': {}           // Minifies CSS in production
+  },
+};
